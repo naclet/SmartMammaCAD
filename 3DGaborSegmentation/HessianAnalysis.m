7dcdea4cb03bf7d901018dd5b7fac1cb1d9c81df
naclet <illan@ugr.es>
2017-11-17 11:19:47 +0100

    output for HessianAnalysis

--- 3DGaborSegmentation/HessianAnalysis.m
+++ 3DGaborSegmentation/HessianAnalysis.m
@@ -9,9 +9,9 @@ detHess(i,j,k)=det(Hess);
 eigvectsort=sort(abs(diag(eival)));
 if ~isreal(eigvect), eigvectsort=[0 0 1]; end
 aut1(i,j,k)=eigvectsort(1);aut2(i,j,k)=eigvectsort(2);aut3(i,j,k)=eigvectsort(3);
-avec1(i,j,k)=eigvect(1,1);avec2(i,j,k)=eigvect(2,1);avec3(i,j,k)=eigvect(3,1);
-R1(i,j,k)=eigvectsort(2)/(eigvectsort(3)+eps);
-R2(i,j,k)=eigvectsort(1)/(sqrt(abs(eigvectsort(2)*eigvectsort(3)))+eps);
+avec1(i,j,k)=eigvect(1,3);avec2(i,j,k)=eigvect(2,3);avec3(i,j,k)=eigvect(3,3);
+R1(i,j,k)=eigvectsort(1)/(eigvectsort(2)+eps);
+R2(i,j,k)=eigvectsort(3)/(sqrt(abs(eigvectsort(1)*eigvectsort(2)))+eps);
 end
 end
 end
