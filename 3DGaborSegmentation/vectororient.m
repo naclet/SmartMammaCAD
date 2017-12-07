63e71512058ac1f7b388fc2312a3cf9369d50557
naclet <illan@ugr.es>
2017-12-07 17:04:48 +0100

    Vectororient with extension on the borders

--- 3DGaborSegmentation/vectororient.m
+++ 3DGaborSegmentation/vectororient.m
@@ -1,6 +1,6 @@
-% FILTERORIENT - Estimates the local orientation of vectors in a 3D image
+% VECTORORIENT - Estimates the local orientation of vectors in a 3D image
 %
-% Usage:  [vect_orient] = filterorient(im, gradientsigma,...
+% Usage:  [vect_orient] = vectororient(im, gradientsigma,...
 %                                             blocksigma, ...
 %                                             orientsmoothsigma)
 %
@@ -12,9 +12,13 @@
 %             orientsmoothsigma - Sigma of the Gaussian used to smooth
 %                                 the final orientation vector field. 
 %                                 Optional: if ommitted it defaults to 0
+% Output:     vect_orient       - A cell of the same size as im, defining a
+%                                 3D vector for each point in the space
+%
+% 2017 - Ignacio Alvarez Illan - illan [at] ugr [dot] es
 
 function [vect_orient] = ...
-             filterorient(im, gradientsigma, blocksigma, orientsmoothsigma)
+             vectororient(im, gradientsigma, blocksigma, orientsmoothsigma)
         
     if ~exist('orientsmoothsigma', 'var'), orientsmoothsigma = 0; end
     % Size of the image
@@ -31,10 +35,25 @@ function [vect_orient] = ...
     Gy = imfilter(im, fy);fprintf('.')
     Gz = imfilter(im, fz);fprintf('.')
     
-    for i=1:(X-sze)
-        for j=1:(Y-sze)
-            for k=1:(Z-sze)
-                vect_orient{i,j,k}=average_moment(Gx(i:i+sze,j:j+sze,k:k+sze),Gy(i:i+sze,j:j+sze,k:k+sze),Gz(i:i+sze,j:j+sze,k:k+sze));
+    % Extend the images to average on the borders. Add a margin of size sze
+    % at the beginning and at the end
+    Gx_ext=zeros(size(Gx)+2*sze);
+    Gx_ext(1+sze:end-sze,1+sze:end-sze,1+sze:end-sze)=Gx;
+    Gy_ext=zeros(size(Gy)+2*sze);
+    Gy_ext(1+sze:end-sze,1+sze:end-sze,1+sze:end-sze)=Gy;
+    Gz_ext=zeros(size(Gz)+2*sze);
+    Gz_ext(1+sze:end-sze,1+sze:end-sze,1+sze:end-sze)=Gz;
+    
+    
+    for i=(1+sze):X
+        for j=(1+sze):Y
+            for k=(1+sze):Z
+                % Define intervals to average
+                i_int = i-sze:i+sze;
+                j_int = j-sze:j+sze;
+                k_int = k-sze:k+sze;
+                
+                vect_orient{i-sze,j-sze,k-sze}=average_moment(Gx_ext(i_int,j_int,k_int),Gy_ext(i_int,j_int,k_int),Gz_ext(i_int,j_int,k_int));
             end
         end
         fprintf('.')
@@ -46,8 +65,9 @@ end
     M=zeros(3);
     ni=numel(Gx);
     for i=1:ni
-        g_vect=[Gx(i) Gy(i) Gz(i)];
-        M=M+g_vect*g_vect';
+        vect=[Gx(i) Gy(i) Gz(i)];
+%        norm_vect=vect/norm(vect);
+        M=M+vect'*vect;
     end
     [~,eigval,eigvect]=eig(M);
     [~,max_eigval]=max(eigval(:));
