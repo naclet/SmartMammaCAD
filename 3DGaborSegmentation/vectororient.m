031d9bde8e2ebdaf8b15516f42b1553cee9cb1b9
naclet <illan@ugr.es>
2017-12-07 18:41:20 +0100

    Vectororient working

--- 3DGaborSegmentation/vectororient.m
+++ 3DGaborSegmentation/vectororient.m
@@ -9,25 +9,22 @@
 %                                 used to compute image gradients.
 %             blocksigma        - Sigma of the Gaussian weighting used to
 %                                 sum the gradient moments.
-%             orientsmoothsigma - Sigma of the Gaussian used to smooth
-%                                 the final orientation vector field. 
-%                                 Optional: if ommitted it defaults to 0
 % Output:     vect_orient       - A cell of the same size as im, defining a
 %                                 3D vector for each point in the space
 %
 % 2017 - Ignacio Alvarez Illan - illan [at] ugr [dot] es
 
-function [vect_orient] = ...
-             vectororient(im, gradientsigma, blocksigma, orientsmoothsigma)
+function [u,v,w] = ...
+             vectororient(im, gradientsigma, sze)
         
-    if ~exist('orientsmoothsigma', 'var'), orientsmoothsigma = 0; end
+
     % Size of the image
     [X, Y, Z] = size(im);
     
     % Calculate image gradients.
-    sze = fix(6*gradientsigma);   if ~mod(sze,2); sze = sze+1; end
+    sze_g = fix(6*gradientsigma);   if ~mod(sze_g,2); sze_g = sze_g+1; end
 
-    f = images.internal.createGaussianKernel([gradientsigma gradientsigma gradientsigma],[sze sze sze]); % Generate Gaussian filter.
+    f = images.internal.createGaussianKernel([gradientsigma gradientsigma gradientsigma],[sze_g sze_g sze_g]); % Generate Gaussian filter.
     [fx,fy,fz] = gradient(f);          
     
     % Gradient of Gausian.
@@ -53,7 +50,11 @@ function [vect_orient] = ...
                 j_int = j-sze:j+sze;
                 k_int = k-sze:k+sze;
                 
-                vect_orient{i-sze,j-sze,k-sze}=average_moment(Gx_ext(i_int,j_int,k_int),Gy_ext(i_int,j_int,k_int),Gz_ext(i_int,j_int,k_int));
+                % Vector calculation
+                vect_orient=average_moment(Gx_ext(i_int,j_int,k_int),Gy_ext(i_int,j_int,k_int),Gz_ext(i_int,j_int,k_int));
+                u(i-sze,j-sze,k-sze)=vect_orient(1);
+                v(i-sze,j-sze,k-sze)=vect_orient(2);
+                w(i-sze,j-sze,k-sze)=vect_orient(3);
             end
         end
         fprintf('.')
